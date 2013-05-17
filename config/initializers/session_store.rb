0e63cb2fa5621a32b582f1cf01f8b16f4d965c69
Mathieu MARACHE <mathieu@marache.com>
2013-05-17 18:18:44 +0200

    initial rails creation

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AppcastServer::Application.config.session_store :cookie_store, key: '_appcast-server_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AppcastServer::Application.config.session_store :active_record_store
