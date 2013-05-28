c8e19cb625a4d9a8b784798a62ff9e4633120312
Mathieu MARACHE <mathieu@marache.com>
2013-05-28 11:52:40 +0200

    Added invitation code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,21 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable :recoverable,
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  attr_accessor :invite_code
+  attr_accessible :invite_code
+
+  validates_each :invite_code, :on => :create do |record, attr, value|
+    record.errors.add attr, "Please enter correct invite code" unless
+        value && value == "12345"
+  end
 
   has_many :products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130523081629) do
+ActiveRecord::Schema.define(:version => 20130528091754) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(:version => 20130523081629) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "invite_code"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
