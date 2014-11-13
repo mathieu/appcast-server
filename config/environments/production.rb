ef18f6542057f962ddd5ea74766d21090711bde1
Mathieu MARACHE <mathieu@marache.com>
2014-11-13 15:00:33 +0100

    testing sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,8 @@ AppcastServer::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => ENV['APPCAST_MAIL_HOST'] || 'localhost' }
-
+  config.action_mailer.delivery_method = :sendmail
+  
   config.relative_url_root = '/appcast'
   #config.assets.prefix = '/appcast/assets'
 
