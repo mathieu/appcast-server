09f61f707749e676b23f052c7760c958c104e494
Mathieu MARACHE <mathieu@marache.com>
2014-12-01 12:07:16 +0100

    trying to correct appcast links in mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,9 @@ AppcastServer::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => ENV['APPCAST_MAIL_HOST'] || 'localhost' }
+  config.action_mailer.default_url_options = { 
+    :host => ENV['APPCAST_MAIL_HOST'] || 'localhost', 
+    :script_name => "/appcast"}
   config.action_mailer.delivery_method = :sendmail
   
   config.relative_url_root = '/appcast'
