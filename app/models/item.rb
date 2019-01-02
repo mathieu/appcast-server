b707b94e65837e305b83f65e6b689fe463ec2b8c
Mathieu Marache <mathieu@marache.com>
2019-01-02 13:44:21 +0100

    Added validation of attachemnts

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,10 @@ class Item < ActiveRecord::Base
   attr_accessible :product_id, :enclosure_file_name, :enclosure_content_type, :enclosure_file_size, :enclosure_updated_at, :slug, :enclosure_mac_file_name, :enclosure_mac_content_type, :enclosure_mac_file_size, :enclosure_mac_updated_at
 
   has_attached_file :enclosure
+  validates_attachment_content_type :enclosure, :content_type => ["application/zip", "application/octet-stream"]
+
   has_attached_file :enclosure_mac
+  validates_attachment_content_type :enclosure_mac, :content_type => ["application/zip", "application/octet-stream"]
 
   belongs_to :product
 
